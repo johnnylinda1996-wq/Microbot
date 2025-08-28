12e2ab120bf05b6c7218a28ee53fede6444d6f4f
Johnny <johnnyplantinga1996@gmail.com>
2025-08-28 12:42:04 +0200

    test

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/accountselector/AutoLoginConfig.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/accountselector/AutoLoginConfig.java
@@ -16,15 +16,6 @@ public interface AutoLoginConfig extends Config {
     String generalSection = "general";
 
     @ConfigItem(
-            keyName = "World",
-            name = "World",
-            description = "World",
-            position = 0,
-            section = generalSection
-    )
-    default int world() { return 360; }
-
-    @ConfigItem(
             keyName = "Is Member",
             name = "Is Member",
             description = "use Member worlds",
@@ -34,11 +25,39 @@ public interface AutoLoginConfig extends Config {
     default boolean isMember() { return false; }
 
     @ConfigItem(
+            keyName = "World",
+            name = "World",
+            description = "World",
+            position = 1,
+            section = generalSection
+    )
+    default int world() { return 360; }
+
+    @ConfigItem(
             keyName = "RandomWorld",
             name = "RandomWorld",
             description = "use random worlds",
-            position = 0,
+            position = 2,
             section = generalSection
     )
     default boolean useRandomWorld() { return true; }
+
+    @ConfigItem(
+            keyName = "WaitBeforeLogin",
+            name = "Wait Before logging in?",
+            description = "Waits the given minutes before logging in.",
+            position = 3,
+            section = generalSection
+    )
+    default boolean WaitBeforeLogin() { return false; }
+
+    @ConfigItem(
+            keyName = "MinutesbeforeLogin",
+            name = "Minutes before logging in",
+            description = "Enter how much minutes to wait before logging back in.",
+            position = 4,
+            section = generalSection
+    )
+    default int MinutesbeforeLogin() { return 20; }
+
 }
--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/accountselector/AutoLoginScript.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/accountselector/AutoLoginScript.java
@@ -9,25 +9,68 @@ import java.util.concurrent.TimeUnit;
 
 public class AutoLoginScript extends Script {
 
+    private long loginAttemptTime = -1;
+    private boolean waitingToLogin = false;
+
     public boolean run(AutoLoginConfig autoLoginConfig) {
         mainScheduledFuture = scheduledExecutorService.scheduleWithFixedDelay(() -> {
             try {
                 if (!super.run()) return;
 
                 if (Microbot.getClient().getGameState() == GameState.LOGIN_SCREEN) {
-                    if (autoLoginConfig.useRandomWorld()) {
-                        new Login(Login.getRandomWorld(autoLoginConfig.isMember()));
-                    } else {
-                        new Login(autoLoginConfig.world());
-                    }
-                    sleep(5000);
-                }
 
+                    // Check if Wait Before Login is enabled
+                    if (autoLoginConfig.WaitBeforeLogin()) {
+                        handleDelayedLogin(autoLoginConfig);
+                    } else {
+                        // Original functionality - direct login
+                        performLogin(autoLoginConfig);
+                    }
+                }
 
             } catch (Exception ex) {
                 Microbot.logStackTrace(this.getClass().getSimpleName(), ex);
             }
         }, 0, 1000, TimeUnit.MILLISECONDS);
         return true;
+    }
+
+    private void handleDelayedLogin(AutoLoginConfig autoLoginConfig) {
+        long currentTime = System.currentTimeMillis();
+
+        // If we haven't started waiting yet, set the login time
+        if (loginAttemptTime == -1) {
+            loginAttemptTime = currentTime + (autoLoginConfig.MinutesbeforeLogin() * 60 * 1000L);
+            waitingToLogin = true;
+            Microbot.log("Waiting " + autoLoginConfig.MinutesbeforeLogin() + " minutes before logging in...");
+        }
+
+        // Check if it's time to login
+        if (waitingToLogin && currentTime >= loginAttemptTime) {
+            Microbot.log("Wait period completed. Logging in now...");
+            performLogin(autoLoginConfig);
+
+            // Reset the wait state
+            loginAttemptTime = -1;
+            waitingToLogin = false;
+        } else if (waitingToLogin) {
+            // Still waiting - show remaining time every 30 seconds
+            long remainingTime = loginAttemptTime - currentTime;
+            long remainingMinutes = remainingTime / (60 * 1000);
+
+            if (remainingTime % 30000 < 1000) { // Every ~30 seconds
+                Microbot.log("Login in " + remainingMinutes + " minutes and " +
+                        ((remainingTime % (60 * 1000)) / 1000) + " seconds...");
+            }
+        }
+    }
+
+    private void performLogin(AutoLoginConfig autoLoginConfig) {
+        if (autoLoginConfig.useRandomWorld()) {
+            new Login(Login.getRandomWorld(autoLoginConfig.isMember()));
+        } else {
+            new Login(autoLoginConfig.world());
+        }
+        sleep(5000);
     }
 }
